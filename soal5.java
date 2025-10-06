296d6fa14b1a4757451ccf3356ba68d093f8cbd6
andimuhammad4409-hash <andi.muhammad4409@smk.belajar.id>
2025-10-07 01:10:48 +0800

    Create soal5.java

---
+++ soal5.java
@@ -0,0 +1,12 @@
+public class soal5{
+  public static void main(String[] args){
+    int angka_awal = 1;
+
+    while (angka_awal <= 100){
+      if (angka_awal % 7 == 0) {
+        System.out.println(angka_awal);
+      }
+      angka_awal++;
+    }
+  }
+}
