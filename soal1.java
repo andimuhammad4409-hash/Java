4a2789446772a9fc44244c1336e6d29ffe0fc2d0
andimuhammad4409-hash <andi.muhammad4409@smk.belajar.id>
2025-10-07 01:40:48 +0800

    Create soal1.java

---
+++ soal1.java
@@ -0,0 +1,11 @@
+public class soal1{
+  public static void main(String[] args){
+    int kantong = 3;
+
+    for (int i = 1 ;i <= 10;i++) {
+      System.out.println("kantong ke-"+i+" kelereng "+ kantong);
+
+      kantong += 2;
+    }
+  }
+}
